17aba37a4cf980be3142cd58321b4fd65376ef51
amitj-coder <amitj-coder@users.noreply.github.com>
2020-11-01 12:51:29 +0530

    updated

--- home/urls.py
+++ home/urls.py
@@ -5,8 +5,22 @@ from django.conf import settings
 from. import views
 urlpatterns = [
     path('', views.food_item, name='home'),
+
+    path('menu/', views.menu_item, name='menu'),
+    path('menu2/', views.menu_item_2, name='menu-2'),
+    path('menu3/', views.menu_item_3, name='menu-3'),
+
     path('add-to-cart/<slug>/', views.add_to_cart, name='add-to-cart'),
     path('remove-from-cart/<slug>/', views.remove_single_item_from_cart, name='remove-from-cart'),
+
+    path('add-to-cart-menu/<slug>/', views.add_to_cart_menu, name='add-to-cart-menu'),
+    path('remove-from-cart-menu/<slug>/', views.remove_single_item_from_cart_menu, name='remove-from-cart-menu'),
+
+    path('add-to-cart-menu2/<slug>/', views.add_to_cart_menu2, name='add-to-cart-menu2'),
+    path('remove-from-cart-menu2/<slug>/', views.remove_single_item_from_cart_menu2, name='remove-from-cart-menu2'),
+
+    path('add-to-cart-menu3/<slug>/', views.add_to_cart_menu3, name='add-to-cart-menu3'),
+    path('remove-from-cart-menu3/<slug>/', views.remove_single_item_from_cart_menu3, name='remove-from-cart-menu3'),
 
 ]
 if settings.DEBUG:
--- home/views.py
+++ home/views.py
@@ -11,15 +11,56 @@ from .models import FoodOrder, FoodItem, PlateItems
 
 
 def food_item(request):
-    menu = FoodItem.objects.all()
+    menu = FoodItem.objects.all()[:4]
     order = PlateItems.objects.filter(user=request.user, ordered=False)
+    full_order = FoodOrder.objects.get(user=request.user, ordered=False)
     context = {
         'menu': menu,
         'order_items': order,
+        'order_items_count': order.count(),
+        'order': full_order,
     }
     return render(request, 'home/index.html', context)
 
 
+def menu_item(request):
+    menu = FoodItem.objects.all()
+    order = PlateItems.objects.filter(user=request.user, ordered=False)
+    full_order = FoodOrder.objects.get(user=request.user, ordered=False)
+    context = {
+        'menu': menu,
+        'order_items': order,
+        'order_items_count': order.count(),
+        'order': full_order,
+    }
+    return render(request, 'home/menu.html', context)
+
+
+def menu_item_2(request):
+    menu = FoodItem.objects.all()
+    order = PlateItems.objects.filter(user=request.user, ordered=False)
+    full_order = FoodOrder.objects.get(user=request.user, ordered=False)
+    context = {
+        'menu': menu,
+        'order_items': order,
+        'order_items_count': order.count(),
+        'order': full_order,
+    }
+    return render(request, 'home/menu2.html', context)
+
+
+def menu_item_3(request):
+    menu = FoodItem.objects.all()
+    order = PlateItems.objects.filter(user=request.user, ordered=False)
+    full_order = FoodOrder.objects.get(user=request.user, ordered=False)
+
+    context = {
+        'menu': menu,
+        'order_items': order,
+        'order_items_count': order.count(),
+        'order': full_order,
+    }
+    return render(request, 'home/menu3.html', context)
 # class FoodItemView(ListView):
 #     model = FoodItem
 #     paginate_by = 5
@@ -80,7 +121,7 @@ def add_to_cart(request, slug):
         order.items.add(order_item)
         order.save()
         messages.info(request, "Item was added to your cart.")
-    return redirect('/', slug=item.id)
+    return redirect('/')
 
 
 @login_required
@@ -98,24 +139,217 @@ def remove_single_item_from_cart(request, slug):
                 user=request.user,
                 ordered=False
             )[0]
-            if order_item.quantity > 1:
-                order_item.quantity -= 1
-                order_item.save()
-            else:
-                try:
-                    mini_order = PlateItems.objects.get(item=item, ordered=False, user=request.user)
-                    mini_order.delete()
-                except ObjectDoesNotExist:
-                    pass
-                order.items.remove(order_item)
+            try:
+                mini_order = PlateItems.objects.get(item=item, ordered=False, user=request.user)
+                mini_order.delete()
+            except ObjectDoesNotExist:
+                pass
+            order.items.remove(order_item)
             messages.info(request, "This item quantity was updated.")
             return redirect("/")
         else:
             # add a message saying the user dosent have an order
             messages.info(request, "Item was not in your cart.")
-            return redirect("/", slug=slug)
+            return redirect("/")
     else:
         # add a message saying the user dosent have an order
         messages.info(request, "You don't have an active order.")
-        return redirect("/", slug=slug)
+        return redirect("/")
 
+
+@login_required
+def add_to_cart_menu(request, slug):
+    item = get_object_or_404(FoodItem, slug=slug)
+    order_item, created = PlateItems.objects.get_or_create(
+        item=item,
+        user=request.user,
+        ordered=False,
+    )
+    order_qs = FoodOrder.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+            messages.info(request, "Item qty was updated.")
+            return redirect("menu")
+        else:
+            order.items.add(order_item)
+            order.save()
+
+    else:
+        ordered_date = timezone.datetime.now().strftime('%Y-%m-%d')
+        ordered_time = timezone.datetime.now().strftime('%H:%M:%S')
+        order = FoodOrder.objects.create(
+            user=request.user, ordered_date=ordered_date, ordered_time=ordered_time)
+        ORN = f"ORN-{100000 + int(order.id)}"
+        order.order_ref_number = ORN
+        order.items.add(order_item)
+        order.save()
+        messages.info(request, "Item was added to your cart.")
+    return redirect('menu')
+
+
+@login_required
+def remove_single_item_from_cart_menu(request, slug):
+    item = get_object_or_404(FoodItem, slug=slug)
+    order_qs = FoodOrder.objects.filter(
+        user=request.user,
+        ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = PlateItems.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            try:
+                mini_order = PlateItems.objects.get(item=item, ordered=False, user=request.user)
+                mini_order.delete()
+            except ObjectDoesNotExist:
+                pass
+            order.items.remove(order_item)
+            messages.info(request, "This item quantity was updated.")
+            return redirect("menu")
+        else:
+            # add a message saying the user dosent have an order
+            messages.info(request, "Item was not in your cart.")
+            return redirect("menu")
+    else:
+        # add a message saying the user dosent have an order
+        messages.info(request, "You don't have an active order.")
+        return redirect("menu")
+
+
+@login_required
+def add_to_cart_menu2(request, slug):
+    item = get_object_or_404(FoodItem, slug=slug)
+    order_item, created = PlateItems.objects.get_or_create(
+        item=item,
+        user=request.user,
+        ordered=False,
+    )
+    order_qs = FoodOrder.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+            messages.info(request, "Item qty was updated.")
+            return redirect("menu-2")
+        else:
+            order.items.add(order_item)
+            order.save()
+
+    else:
+        ordered_date = timezone.datetime.now().strftime('%Y-%m-%d')
+        ordered_time = timezone.datetime.now().strftime('%H:%M:%S')
+        order = FoodOrder.objects.create(
+            user=request.user, ordered_date=ordered_date, ordered_time=ordered_time)
+        ORN = f"ORN-{100000 + int(order.id)}"
+        order.order_ref_number = ORN
+        order.items.add(order_item)
+        order.save()
+        messages.info(request, "Item was added to your cart.")
+    return redirect('menu-2', slug=item.id)
+
+
+@login_required
+def remove_single_item_from_cart_menu2(request, slug):
+    item = get_object_or_404(FoodItem, slug=slug)
+    order_qs = FoodOrder.objects.filter(
+        user=request.user,
+        ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = PlateItems.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            try:
+                mini_order = PlateItems.objects.get(item=item, ordered=False, user=request.user)
+                mini_order.delete()
+            except ObjectDoesNotExist:
+                pass
+            order.items.remove(order_item)
+            messages.info(request, "This item quantity was updated.")
+            return redirect("menu-2")
+        else:
+            # add a message saying the user dosent have an order
+            messages.info(request, "Item was not in your cart.")
+            return redirect("menu-2")
+    else:
+        # add a message saying the user dosent have an order
+        messages.info(request, "You don't have an active order.")
+        return redirect("menu-2")
+
+
+@login_required
+def add_to_cart_menu3(request, slug):
+    item = get_object_or_404(FoodItem, slug=slug)
+    order_item, created = PlateItems.objects.get_or_create(
+        item=item,
+        user=request.user,
+        ordered=False,
+    )
+    order_qs = FoodOrder.objects.filter(user=request.user, ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item.quantity += 1
+            order_item.save()
+            messages.info(request, "Item qty was updated.")
+            return redirect("menu-3")
+        else:
+            order.items.add(order_item)
+            order.save()
+
+    else:
+        ordered_date = timezone.datetime.now().strftime('%Y-%m-%d')
+        ordered_time = timezone.datetime.now().strftime('%H:%M:%S')
+        order = FoodOrder.objects.create(
+            user=request.user, ordered_date=ordered_date, ordered_time=ordered_time)
+        ORN = f"ORN-{100000 + int(order.id)}"
+        order.order_ref_number = ORN
+        order.items.add(order_item)
+        order.save()
+        messages.info(request, "Item was added to your cart.")
+    return redirect('menu-3')
+
+
+@login_required
+def remove_single_item_from_cart_menu3(request, slug):
+    item = get_object_or_404(FoodItem, slug=slug)
+    order_qs = FoodOrder.objects.filter(
+        user=request.user,
+        ordered=False)
+    if order_qs.exists():
+        order = order_qs[0]
+        # check if the order item is in the order
+        if order.items.filter(item__slug=item.slug).exists():
+            order_item = PlateItems.objects.filter(
+                item=item,
+                user=request.user,
+                ordered=False
+            )[0]
+            try:
+                mini_order = PlateItems.objects.get(item=item, ordered=False, user=request.user)
+                mini_order.delete()
+            except ObjectDoesNotExist:
+                pass
+            order.items.remove(order_item)
+            messages.info(request, "This item quantity was updated.")
+            return redirect("menu-3")
+        else:
+            # add a message saying the user dosent have an order
+            messages.info(request, "Item was not in your cart.")
+            return redirect("menu-3")
+    else:
+        # add a message saying the user dosent have an order
+        messages.info(request, "You don't have an active order.")
+        return redirect("menu-3")
