3bfe76a046e0deccaf33305855b6c44ac6985f08
Aniket <aniket.dg25@gmail.com>
2021-06-20 17:38:36 +0530

    Upcoming event model added

--- home/models.py
+++ home/models.py
@@ -29,3 +29,10 @@ class TPP(models.Model):
         return 'TPP'
 
 
+class UpcomingEvent(models.Model):
+    title = models.CharField(max_length=100)
+    date = models.DateField()
+    description = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{ self.title }"
